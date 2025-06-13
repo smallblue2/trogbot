db83ef42812f408bc5cc1b65b7483042b77a472e
OshDubh <oshdubh@gmail.com>
2025-06-13 19:44:24 +0100

    added marker world option choice

--- minecraft/dynmap.go
+++ minecraft/dynmap.go
@@ -4,13 +4,14 @@ Description: interfaces with dynmap marker commands to allow for the
              creation, modification and deletion of dynmap markers
 Created by: osh
         at: 15:39 on Friday, the 13th of June, 2025.
-Last edited 17:30 on Friday, the 13th of June, 2025.
+Last edited 19:33 on Friday, the 13th of June, 2025.
 */
 
 package minecraft
 
 type Marker struct {
 	Label     string
+	Id        string
 	Icon      string
 	Set       string
 	XCoord    int
@@ -33,11 +34,32 @@ type MarkerSet struct {
 	DefaultIcon string
 }
 
+type MarkerWorld struct {
+	Name         string
+	Loaded       bool
+	Enabled      bool
+	Title        string
+	Center       string
+	ExtraZoomOut int
+	SendHealth   bool
+	SendPosition bool
+	Protected    bool
+	ShowBorder   bool
+}
+
 // adds a marker given marker fields
 func AddMarker(marker Marker) error
 
 // builds the `/dmarker add` command, allowing for optional fields
 func buildAddMarker(marker Marker) (string, error)
+
+// retrieves the currently available worlds using `/dmap worldlist` to populat ethe slash command options
+// looks like (newline seperated):
+//
+// world <world id>: loaded=<bool>, enabled=<bool>, title=<the dimension name>, center=<x.x/y.y/z.z>, extrazoomout=<int>, sendpositon=<bool>, protected=<bool>, showborder=<bool>
+// eg:
+// world world_172429123: loaded=true, enabled=true, title=world, center=-32.0/70.0/-80.0, extrazoomout=2, sendhealth=true, sendposition=true, protected=false, showborder=true
+func GetMarkerWorlds() ([]MarkerWorld, error)
 
 // retrieves currently available marker icons using `/dmarker icons` to populate the slash command options
 // looks like (newline seperated):
